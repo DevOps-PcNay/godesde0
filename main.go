d5a961d1d7106d66ea202018346aa4840181082e
Soporte <progsop@srv645335.hstgr.cloud>
2024-12-18 19:40:06 -0800

    Primer commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	// Export GOPATH = /var/www/html/soporte.my-empresa.com/github.com/DevOps-PcNay/godesde0 
+	"godesde0/variables"
+)
+
+//"fmt",
+
+func main() {
+	//fmt.Println("Hola mundo")
+	variables.MuestroEnteros()
+}
---
+++ variables/enteras.go
@@ -0,0 +1,20 @@
+package variables
+import (
+	"fmt")
+
+func MuestroEnteros() {
+	// Declativa 
+	// Asignacion
+
+	// Declaracion
+	var intcomun int // Valor por defecto es el minimo
+	
+
+	// Asignacion
+	intde32 := int32(10)
+	intde64 := int64(100)
+
+	fmt.Println("intcomun = ",intcomun)
+	fmt.Println("intde32 = ",intde32)
+	fmt.Println("intde64 = ",intde64)
+}
